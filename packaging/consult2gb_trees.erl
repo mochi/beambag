023156f844997b89ce555198fba0363e8627efce
Igor Milyakov <imilyakov@machinezone.com>
2014-06-05 16:28:29 -0700

    deal with single element tuple

--- packaging/consult2gb_trees.erl
+++ packaging/consult2gb_trees.erl
@@ -1,4 +1,8 @@
 fun(SourceFN) when is_list(SourceFN) ->
     {ok, Terms} = file:consult(SourceFN),
-    gb_trees:from_orddict(lists:usort(Terms))
+    Terms2 = case hd(Terms) of
+                 {K} -> [{K1, []} || {K1} <- Terms];
+                 _ -> Terms
+             end,
+    gb_trees:from_orddict(lists:usort(Terms2))
 end.
